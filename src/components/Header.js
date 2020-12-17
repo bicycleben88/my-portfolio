e17b7173132b047c03e531c080376a1a4c771fa0
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-12-17 15:19:34 -0500

    style nav bar and add toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,36 @@
 import React from "react"
-import { graphql, Link, useStaticQuery } from "gatsby"
+import ToggleDisplay from "react-toggle-display"
 import HeaderStyles from "../styles/headerstyles"
 
 const Header = props => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `
-  )
+  const [display, setDisplay] = React.useState({ show: false })
+
+  const handleToggle = () => {
+    setDisplay({ show: !display.show })
+  }
+
   return (
     <HeaderStyles>
       <img src="https://i.imgur.com/ihf8iIi.jpg?1" alt="Head Shot of Ben" />
-      <div>
-        <h1>{data.site.siteMetadata.title}</h1>
-        <h2>{data.site.siteMetadata.description}</h2>
+      <div className="title">
+        <h1>Benjamin A Higginbotham.</h1>
+        <h2>I ride bikes in Lycra.</h2>
+        <h2>I write code in JavaScript.</h2>
       </div>
       <nav>
-        <div>Phone</div>
-        <div>Email</div>
-        <div>Git</div>
+        <div className="contact">
+          <h3 onClick={() => handleToggle()}>Contact</h3>
+          <ToggleDisplay show={display.show}>
+            <h4>(352)642-6701</h4>
+            <h4> bicycleben88@gmail.com</h4>
+          </ToggleDisplay>
+        </div>
+        <a
+          href="https://www.linkedin.com/in/benjamin-alt-higginbotham/"
+          target="#"
+        >
+          <h3>LinkedIn</h3>
+        </a>
       </nav>
     </HeaderStyles>
   )
