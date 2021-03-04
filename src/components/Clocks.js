81b3dc3413e5075255b3926a1f41ebc669f6ddb1
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-03 23:14:57 -0500

    clock functions

--- src/components/Clocks.js
+++ src/components/Clocks.js
@@ -25,13 +25,53 @@ const Clock = styled.div`
   }
 `
 export default function Clocks() {
+  const [seconds, setSeconds] = React.useState(null)
+  const [minutes, setMinutes] = React.useState(null)
+  const [hour, setHour] = React.useState(null)
+
+  const css = {
+    seconds: { transform: `rotate(${(seconds / 60) * 360 + 90}deg)` },
+    minutes: { transform: `rotate(${(minutes / 60) * 360 + 90}deg)` },
+    hour: { transform: `rotate(${(hour / 12) * 360 + 90}deg)` },
+  }
+
+  function getSeconds() {
+    setInterval(() => {
+      const date = new Date()
+      const seconds = date.getSeconds()
+      setSeconds(seconds)
+    }, 1000)
+  }
+
+  function getMinutes() {
+    setInterval(() => {
+      const date = new Date()
+      const minutes = date.getMinutes()
+      setMinutes(minutes)
+    }, 1000)
+  }
+
+  function getHour() {
+    setInterval(() => {
+      const date = new Date()
+      const hour = date.getHours()
+      setHour(hour)
+    }, 1000)
+  }
+
+  React.useEffect(() => {
+    getSeconds()
+    getMinutes()
+    getHour()
+  }, [])
+
   return (
-    <div className="clocks">
+    <div className="clocks" id="clocks">
       <Clock>
         <div className="face">
-          <div class="hand hour-hand"></div>
-          <div class="hand min-hand"></div>
-          <div class="hand second-hand"></div>
+          <div class="hand hour-hand" style={css.hour}></div>
+          <div class="hand min-hand" style={css.minutes}></div>
+          <div class="hand second-hand" style={css.seconds}></div>
         </div>
       </Clock>
       <Clock>
