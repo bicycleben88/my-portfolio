a0fce4bd4433a972b4051a8fe80c2a01acc6bcda
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-06 14:00:49 -0500

    remove global styled component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,18 +2,10 @@ import React from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import "../styles/global.css"
-import { createGlobalStyle } from "styled-components"
-
-const GlobalStyles = createGlobalStyle`
-  html {
-    --tilt: -10;
-  }
-`
 
 const Layout = props => {
   return (
     <>
-      <GlobalStyles />
       <Header />
       <main>{props.children}</main>
       <Footer />
