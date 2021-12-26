025dfe2d5081aa272a6fc211f54ce5713a922735
Ben Higginbotham <bicycleben88@gmail.com>
2021-12-26 11:16:00 -0500

    add links to individual pagination pages

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -19,6 +19,15 @@ export default function Pagination({
       <Link to={`${base}/${prevPage}`} disabled={!hasPrevPage}>
         &#8592; Back
       </Link>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <Link
+          to={`${base}/${i > 0 ? i + 1 : ""}`}
+          className={currentPage === 1 && i === 0 ? "current" : ""}
+          key={Math.random()}
+        >
+          {i + 1}
+        </Link>
+      ))}
       <Link to={`${base}/${nextPage}`} disabled={!hasNextPage}>
         Next &#8594;
       </Link>
