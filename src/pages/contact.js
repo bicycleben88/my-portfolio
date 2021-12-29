5daa0d180914e0ee91aedc7ca4f240e3b8a9fec2
Ben Higginbotham <bicycleben88@gmail.com>
2021-12-29 17:55:36 -0500

    create custom useForm hook to handle form changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,15 @@
 import React from "react"
 import SEO from "../components/SEO"
+import useForm from "../utils/useForm"
 
-export default function contact() {
+export default function Contact() {
+  const { values, updateValues } = useForm({
+    name: "",
+    email: "",
+    role: "",
+    query: "",
+  })
+
   return (
     <>
       <SEO title="Contact me" />
@@ -10,11 +18,23 @@ export default function contact() {
           <legend>Your Info</legend>
           <label htmlFor="name">
             Name
-            <input type="text" name="name" id="name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={values.name}
+              onChange={updateValues}
+            />
           </label>
           <label htmlFor="email">
             Email
-            <input type="text" name="email" id="email" />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={updateValues}
+            />
           </label>
           <label htmlFor="role">
             Role
@@ -23,6 +43,8 @@ export default function contact() {
               name="role"
               placeholder="Recruiter, Project Manager, etc"
               id="role"
+              value={values.role}
+              onChange={updateValues}
             />
           </label>
         </fieldset>
@@ -30,7 +52,12 @@ export default function contact() {
           <legend>Inquest Info</legend>
           <label htmlFor="query">
             What can I do for you?
-            <textarea id="query" name="query" />
+            <textarea
+              id="query"
+              name="query"
+              value={values.query}
+              onChange={updateValues}
+            />
           </label>
         </fieldset>
       </form>
--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import kubrick from "typography-theme-wordpress-kubrick"
-kubrick.baseFontSize = "15px"
-
-const typography = new Typography(kubrick)
-
-export const { rhythm, scale, options } = typography
-export default typography
