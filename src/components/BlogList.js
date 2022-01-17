dbd9b774c8cb834d0aa086543874787eab7f5325
Ben Higginbotham <bicycleben88@gmail.com>
2022-01-17 13:43:30 -0500

    styled blogs page

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,22 +1,56 @@
 import { Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
+
+const SingleBlogStyles = styled.div`
+  --tall: 150px;
+  box-shadow: 0 0 5px 1px var(--black);
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1 1 var(--tall);
+  h2 {
+    font-size: 2.5rem;
+  }
+  p {
+    margin: 0;
+  }
+  @media (max-width: 400px) {
+    --tall: 200px;
+    h2 {
+      font-size: 2rem;
+      text-align: center;
+    }
+    p {
+      line-height: 22px;
+      margin-top: 5px;
+    }
+  }
+`
+const BlogListStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
 
 function SingleBlog({ blog }) {
   return (
-    <div>
+    <SingleBlogStyles>
       <Link to={blog.fields.slug}>
         <h2>{blog.frontmatter.title}</h2>
       </Link>
       <p>{blog.excerpt}</p>
-    </div>
+    </SingleBlogStyles>
   )
 }
+
 export default function BlogList({ blogs }) {
   return (
-    <div>
+    <BlogListStyles>
       {blogs.map(blog => (
         <SingleBlog blog={blog} key={blog.id} />
       ))}
-    </div>
+    </BlogListStyles>
   )
 }
